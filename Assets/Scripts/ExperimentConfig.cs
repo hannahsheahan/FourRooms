263e42c731d3a28fa4191cad2e691d1989e028be
Hannah Sheahan <sheahan.hannah@gmail.com>
2018-11-15 15:35:42 +0000

    finally got an effective fog going  that runs smoothly and restricts vision properly

--- Assets/Scripts/ExperimentConfig.cs
+++ Assets/Scripts/ExperimentConfig.cs
@@ -46,6 +46,12 @@ public class ExperimentConfig
     private Vector3[] star1Positions;
     private Vector3[] star2Positions;
 
+    private Vector3[] blueRoomPositions;
+    private Vector3[] redRoomPositions;
+    private Vector3[] yellowRoomPositions;
+    private Vector3[] greenRoomPositions;
+
+
     // Rewards
     private bool[] doubleRewardTask;         // if there are two stars to collect: true, else false
     private const int ONE_STAR = 0;
@@ -73,15 +79,14 @@ public class ExperimentConfig
     {
 
         // Set these variables to define your experiment:
-        practiceTrials = 3   + getReadyTrial;
-        totalTrials    = 2   + setupAndCloseTrials + practiceTrials;        // accounts for the Persistent, StartScreen and Exit 'trials'
+        practiceTrials = 0   + getReadyTrial;
+        totalTrials    = 10   + setupAndCloseTrials + practiceTrials;        // accounts for the Persistent, StartScreen and Exit 'trials'
         restFrequency  = 4    + restbreakOffset;            // Take a rest after this many normal trials
 
         // Figure out how many rest breaks we will have and add them to the trial list
         nbreaks = Math.Max( (int)((totalTrials - setupAndCloseTrials - practiceTrials) / restFrequency), 0 );  // round down to whole integer
         totalTrials = totalTrials + nbreaks;
        
-        // ... ***HRS add other variables to control here
 
 
         // Timer variables (measured in seconds) - these can later be changed to be different per trial for jitter etc
@@ -89,7 +94,7 @@ public class ExperimentConfig
         restbreakDuration   = 5.0f;    // how long are the imposed rest breaks?
         getReadyDuration    = 5.0f;    // how long do we have to 'get ready' after the practice, before main experiment begins?
 
-        maxMovementTime     = 15.0f;
+        maxMovementTime     = 30.0f;
         preDisplayCueTime   = 1.5f;    // will take a TR during this period
         displayCueTime      = 2.0f;
         goCueDelay          = 1.5f;    // will take a TR during this period
@@ -98,11 +103,10 @@ public class ExperimentConfig
         waitFinishTime      = 1.5f;
         errorDwellTime      = 1.5f;    // Note: should be at least as long as displayMessageTime
 
-
         // These variables define the environment (are less likely to be played with)
         roomSize        = 5;           // rooms are each 5x5 grids. If this changes, you will need to change this code
         playerYposition = 72.5f;
-        starYposition   = 75.5f;
+        starYposition   = 73.5f;
         mazeCentre      = new Vector3(145.0f, playerYposition, 145.0f);
 
 
@@ -119,11 +123,7 @@ public class ExperimentConfig
         GeneratePossibleSettings();
 
 
-        // Generate the trial randomisation etc that we want:
-        // **HRS (later we can generate an array of things to cover and then permute from a function here) 
-
-        // Define the first two trials (which are the Persistent and StartScreen states)
-        // Note: this lets the other variables take their default values on these starting trials
+        // Define the start up menu trials.   Note:  the other variables take their default values on these trials
         trialMazes[0] = "Persistent";
         trialMazes[1] = "StartScreen";
         trialMazes[2] = "InstructionsScreen";
@@ -151,54 +151,15 @@ public class ExperimentConfig
         // Define the final exit state
         trialMazes[totalTrials-1] = "Exit";
 
-        // Let's make the trial content completely random for now
-        for (int trial = setupTrials+ practiceTrials; trial < totalTrials-1; trial++)
-        {
-            // Deal with restbreaks and regular trials
-            if (  (trial - setupTrials - practiceTrials + 1) % restFrequency == 0)  // Time for a rest break
-            {  
-                trialMazes[trial] = "RestBreak";
-            }
-            else                                    // It's a regular trial
-            {
-                // For now, change the reward type every second trial (if mod 2)
-                //if (trial % 1 == 0)          
-                if (trial<0)  // for now just fix as 'always cheese'
-                {
-                    rewardTypes[trial] = "wine";    // use single reward type for now
-                }
-                else
-                {
-                    rewardTypes[trial] = "cheese";    // use single reward type for now
-                }
 
-                trialMazes[trial] = "FourRooms_" + rewardTypes[trial];   // set this to stay the same, for now
-                doubleRewardTask[trial] = false;   // use only single star trials for now
+        // Generate the trial randomisation/list that we want
 
-                playerStartPositions[trial] = possiblePlayerPositions[UnityEngine.Random.Range(0, possiblePlayerPositions.Length-1)]; // random start position
-                playerStartOrientations[trial] = findStartOrientation(playerStartPositions[trial]);   // orient player towards the centre of the environment
-                star1Positions[trial] = possibleStarPositions[UnityEngine.Random.Range(0, possibleStarPositions.Length - 1)];           // random star1 position
+        RandomPlayerAndRewardPositions();
 
-                // ensure reward doesnt spawn on the player position (later this will be pre-determined)
-                while (playerStartPositions[trial] == star1Positions[trial])
-                {
-                    star1Positions[trial] = possibleStarPositions[UnityEngine.Random.Range(0, possibleStarPositions.Length - 1)];           // random star1 position
-                }
+        // **HRS (later we can generate an array of things to cover and then permute from a function here) 
 
-
-                if (doubleRewardTask[trial])
-                {   // generate another position for star2
-                    star2Positions[trial] = possibleStarPositions[UnityEngine.Random.Range(0, possibleStarPositions.Length - 1)];     // random star2 position
-                }
-                else
-                {   // single star to be collected
-                    star2Positions[trial] = star1Positions[trial];  
-                }
-            }
-        }
-
-
-        // ... ** HRS add any other randomisation etc you want here
+        //RandomPositionInRoom("blue");
+        //ShuffleTrialOrder();  //***HRS have not written yet
 
     }
 
@@ -209,6 +170,10 @@ public class ExperimentConfig
         // Generate all possible spawn locations for player and stars
         possiblePlayerPositions = new Vector3[roomSize * roomSize * 4]; // we are working with 4 square rooms
         possibleStarPositions = new Vector3[roomSize * roomSize * 4];
+        blueRoomPositions = new Vector3[roomSize * roomSize];
+        redRoomPositions = new Vector3[roomSize * roomSize];
+        yellowRoomPositions = new Vector3[roomSize * roomSize];
+        greenRoomPositions = new Vector3[roomSize * roomSize];
 
         // Blue room
         int startind = 0;
@@ -237,6 +202,13 @@ public class ExperimentConfig
         int[] ZPositionsyellow = { 155, 165, 175, 185, 195 };
         AddPossibleLocations(possiblePlayerPositions, startind, XPositionsyellow, playerYposition, ZPositionsyellow);
         AddPossibleLocations(possibleStarPositions, startind, XPositionsyellow, starYposition, ZPositionsyellow);
+
+        // Add position arrays for locations in particular rooms
+        startind = 0;
+        AddPossibleLocations(blueRoomPositions, startind, XPositionsblue, starYposition, ZPositionsblue);
+        AddPossibleLocations(redRoomPositions, startind, XPositionsred, starYposition, ZPositionsred);
+        AddPossibleLocations(greenRoomPositions, startind, XPositionsgreen, starYposition, ZPositionsgreen);
+        AddPossibleLocations(yellowRoomPositions, startind, XPositionsyellow, starYposition, ZPositionsyellow);
 
         // Get all the possible mazes/scenes in the build that we can work with
         sceneCount = SceneManager.sceneCountInBuildSettings;
@@ -276,6 +248,124 @@ public class ExperimentConfig
             angle = 360 + angle;
         }
         spawnOrientation = new Vector3(0.0f, angle, 0.0f);          return spawnOrientation;     } 
+    // ********************************************************************** //
+
+    private void RandomPlayerAndRewardPositions()
+    {
+
+        // Generate trial content that randomly positions the player and reward/s in the different rooms
+        for (int trial = setupTrials + practiceTrials; trial < totalTrials - 1; trial++)
+        {
+            // Deal with restbreaks and regular trials
+            if ((trial - setupTrials - practiceTrials + 1) % restFrequency == 0)  // Time for a rest break
+            {
+                trialMazes[trial] = "RestBreak";
+            }
+            else                                    // It's a regular trial
+            {
+                // For now, change the reward type every second trial (if mod 2)
+                if (trial % 2 == 0)          
+                //if (trial < 0)  // for now just fix as 'always cheese'
+                {
+                    rewardTypes[trial] = "wine";    // use single reward type for now
+                }
+                else
+                {
+                    rewardTypes[trial] = "cheese";    // use single reward type for now
+                }
+
+                trialMazes[trial] = "FourRooms_" + rewardTypes[trial];   // set this to stay the same, for now
+                doubleRewardTask[trial] = false;   // use only single star trials for now
+
+                playerStartPositions[trial] = possiblePlayerPositions[UnityEngine.Random.Range(0, possiblePlayerPositions.Length - 1)]; // random start position
+                playerStartOrientations[trial] = findStartOrientation(playerStartPositions[trial]);   // orient player towards the centre of the environment
+                star1Positions[trial] = possibleStarPositions[UnityEngine.Random.Range(0, possibleStarPositions.Length - 1)];           // random star1 position
+
+                // ensure reward doesnt spawn on the player position (later this will be pre-determined)
+                while (playerStartPositions[trial] == star1Positions[trial])
+                {
+                    star1Positions[trial] = possibleStarPositions[UnityEngine.Random.Range(0, possibleStarPositions.Length - 1)];           // random star1 position
+                }
+
+
+                if (doubleRewardTask[trial])
+                {   // generate another position for star2
+                    star2Positions[trial] = possibleStarPositions[UnityEngine.Random.Range(0, possibleStarPositions.Length - 1)];     // random star2 position
+                }
+                else
+                {   // single star to be collected
+                    star2Positions[trial] = star1Positions[trial];
+                }
+            }
+        }
+    }
+
+    // ********************************************************************** //
+
+    private Vector3 RandomPositionInRoom(string roomColour)
+    {
+        // select a random position in a room of a given colour
+        switch (roomColour)
+        {
+            case "blue":
+                return blueRoomPositions[UnityEngine.Random.Range(0, blueRoomPositions.Length - 1)];
+
+            case "red":
+                return redRoomPositions[UnityEngine.Random.Range(0, redRoomPositions.Length - 1)];
+
+            case "green":
+                return greenRoomPositions[UnityEngine.Random.Range(0, greenRoomPositions.Length - 1)];
+            
+            case "yellow":
+                return yellowRoomPositions[UnityEngine.Random.Range(0, yellowRoomPositions.Length - 1)];
+            
+            default:
+                return new Vector3(0.0f, 0.0f, 0.0f);  // this should never happen
+        }
+    }
+
+    // ********************************************************************** //
+
+    public void ShuffleTrialOrder( )
+    {
+
+        // ***HRS to write this another day
+
+        string[] p_trialMazes = new string[totalTrials];
+        bool[] p_doubleRewardTask = new bool[totalTrials];
+        Vector3[] p_playerStartPositions = new Vector3[totalTrials];
+        Vector3[] p_playerStartOrientations = new Vector3[totalTrials];
+        Vector3[] p_star1Positions = new Vector3[totalTrials];
+        Vector3[] p_star2Positions = new Vector3[totalTrials];
+
+        // This function will take the existing trial sequence (excluding the menu etc trials), and return a randomly permuted ordering
+
+        for (int trial = setupTrials + practiceTrials; trial < totalTrials - 1; trial++)
+        {
+            if (trialMazes[trial] == "RestBreak")
+            {
+                // save a version of the trial sequence for messing with, so we don't mess with the rest breaks
+               
+
+            }
+        }
+
+        // Shuffle the sequence
+       /*
+        System.Random random = new System.Random();
+        for (int i = elements.Length - 1; i > 0; i--)
+        {
+            int swapIndex = random.Next(i + 1);
+            string tmp = elements[i];
+            elements[i] = elements[swapIndex];
+            elements[swapIndex] = tmp;
+        }
+        */
+        // Put it back together with the rest breaks
+
+    }
+
+
     // ********************************************************************** //
     // Get() and Set() Methods
     // ********************************************************************** //
